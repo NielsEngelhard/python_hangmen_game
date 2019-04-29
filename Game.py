773253b3b73198645ff12f9d493abeed1858ab11
Niels Engelhard <engelhardniels@gmail.com>
2019-04-29 22:44:46 +0200

    Better end of the game when won!

--- Game.py
+++ Game.py
@@ -7,9 +7,12 @@ g = Generator()
 # actual program
 print("Finding a word in the dictionary...")
 game.set_word(g.getWordFromDictionary())
+print(game.hashed_word())
 while not game.wordGuessed:  # while the game is not over
     game.end_of_game()  # check max tries reached
     print("Give letter to guess: ")
     user_try = input()  # input of the guesser
     game.guess(user_try)  # check the guess
 print("end of the game, thr word is: " + str(game.getWord))
+print("press any key to exit")
+input()
